35fdcb48fbedef23b6ddb4cc615ec287e87fe911
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-03-17 19:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function findMajorityElement(arr) {
+  const frequencyMap = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    frequencyMap[element] = (frequencyMap[element] || 0) + 1;
+    if (frequencyMap[element] > arr.length / 2) {
+      return element;
+    }
+  }
+
+  return -1; // no majority element found
+}
